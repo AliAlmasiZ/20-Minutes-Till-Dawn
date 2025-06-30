3d245d57241ff1509d622edc0caf08a3ece9e31e
AliAlmasiZ <Ali1385Almasi@gmail.com>
2025-06-30 16:30:35 +0330

    check password for signup added

--- core/src/main/java/io/github/AliAlmasiZ/tillDawn/controllers/SignupMenuController.java
+++ core/src/main/java/io/github/AliAlmasiZ/tillDawn/controllers/SignupMenuController.java
@@ -5,6 +5,8 @@ import io.github.AliAlmasiZ.tillDawn.models.User;
 import io.github.AliAlmasiZ.tillDawn.models.Result;
 
 import javax.management.InstanceAlreadyExistsException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignupMenuController {
     /*public Result signup(String username, String password, String securityAnswer) {
@@ -40,18 +42,52 @@ public class SignupMenuController {
 
     public Result signup(String username, String password, String securityAnswer) {
 
-        if(AppData.getAppData().getUserByUsername(username) != null) {
-            return new Result(false, "user already exists!");
-        }
+
         try {
+            Result result = checkUsername(username);
+            if(!result.isSuccessful())
+                return result;
+            result =  checkPassword(password);
+            if(!result.isSuccessful())
+                return result;
             User user = new User(username, password, securityAnswer);
             AppData.getAppData().addUser(user);
             AppData.getAppData().setActiveUser(user);
             return new Result(true, "user created successfully!");
-        } catch (InstanceAlreadyExistsException e) {
+        } catch (Exception e) {
             return new Result(false, e.getMessage());
         }
     }
+
+    private  Result checkPassword(String password) {
+        final Pattern special = Pattern.compile(".*(?=[_()*&%$#@]).*");
+        final Pattern number = Pattern.compile(".*(?=\\d).*");
+        final Pattern capitalLetter = Pattern.compile(".*(=?[A-Z]).*");
+
+        if(password.length() < 8)
+            return new Result(false, "password should be at least 8 characters!");
+        if(!special.matcher(password).matches())
+            return new Result(false, "password should have at least one special character");
+        if(!number.matcher(password).matches())
+            return new Result(false, "password should have at least one number");
+        if(!capitalLetter.matcher(password).matches())
+            return new Result(false, "password should have at list one capita letter");
+
+
+        return new Result(true, "");
+    }
+
+    private Result checkUsername(String username) {
+        if(AppData.getAppData().getUserByUsername(username) != null) {
+            return new Result(false, "user already exists!");
+        }
+        if(username.length() < 3)
+            return new Result(false, "username should be at least 3 characters");
+        return new Result(true, "");
+    }
+
+
+
 
     public Result forgetPass() {
         return null;
