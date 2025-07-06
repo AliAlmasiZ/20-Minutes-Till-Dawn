1769d4f019688358b7bbf21c9028a1ebdaedef7c
AliAlmasiZ <Ali1385Almasi@gmail.com>
2025-07-07 00:28:57 +0330

    added preGamge menu

--- core/src/main/java/io/github/AliAlmasiZ/tillDawn/views/PregameMenuView.java
+++ core/src/main/java/io/github/AliAlmasiZ/tillDawn/views/PregameMenuView.java
@@ -1,14 +1,21 @@
 package io.github.AliAlmasiZ.tillDawn.views;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import io.github.AliAlmasiZ.tillDawn.models.Settings;
 import io.github.AliAlmasiZ.tillDawn.models.enums.HeroType;
+import io.github.AliAlmasiZ.tillDawn.models.enums.WeaponType;
 
 public class PregameMenuView {
     Stage stage;
     Table table;
+    public TextButton start;
 
     public PregameMenuView(Skin skin) {
         stage = new Stage(new ScreenViewport());
@@ -16,15 +23,51 @@ public class PregameMenuView {
         table.setFillParent(true);
         stage.addActor(table);
 
-        TextButton start = new TextButton(Text.START_GAME.getText(), skin);
+        start = new TextButton(Text.START_GAME.getText(), skin);
+
         Label heroLabel = new Label(Text.HERO.getText(), skin);
         SelectBox<HeroType> heroSelectBox= new SelectBox<>(skin);
         heroSelectBox.setItems(HeroType.values());
+        heroSelectBox.setSelected(Settings.getInstance().heroType);
+        heroSelectBox.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Settings.getInstance().heroType = heroSelectBox.getSelected();
+                Settings.getInstance().savePrefs();
+            }
+        });
         Label weaponLabel = new Label(Text.WEAPON.getText(), skin);
+        SelectBox<WeaponType> weaponSelectBox = new SelectBox<>(skin);
+        weaponSelectBox.setItems(WeaponType.values());
+        weaponSelectBox.setSelected(Settings.getInstance().weaponType);
+        weaponSelectBox.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Settings.getInstance().weaponType = weaponSelectBox.getSelected();
+                Settings.getInstance().savePrefs();
+            }
+        });
         Label gameDurationLabel = new Label(Text.DURATION.getText(), skin);
+        SelectBox<Settings.Time> timeSelectBox = new SelectBox<>(skin);
+        timeSelectBox.setItems(Settings.Time.values());
+        timeSelectBox.setSelected(Settings.getInstance().gameTime);
+        timeSelectBox.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Settings.getInstance().gameTime = timeSelectBox.getSelected();
+                Settings.getInstance().savePrefs();
+            }
+        });
 
 
-//        table.add()
+
+        table.add(start).colspan(2).row();
+        table.add(heroLabel).pad(20);
+        table.add(heroSelectBox).pad(20).row();
+        table.add(weaponLabel).pad(20);
+        table.add(weaponSelectBox).pad(20).row();
+        table.add(gameDurationLabel).pad(20);
+        table.add(timeSelectBox).pad(20).row();
     }
 
     public void show() {
@@ -34,6 +77,14 @@ public class PregameMenuView {
     public void render(float delta) {
         stage.act(delta);
         stage.draw();
+    }
+
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    public void dispose() {
+
     }
 
 
--- core/src/main/java/io/github/AliAlmasiZ/tillDawn/views/screens/PregameMenuScreen.java
+++ core/src/main/java/io/github/AliAlmasiZ/tillDawn/views/screens/PregameMenuScreen.java
@@ -1,8 +1,12 @@
 package io.github.AliAlmasiZ.tillDawn.views.screens;
 
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import io.github.AliAlmasiZ.tillDawn.Main;
+import io.github.AliAlmasiZ.tillDawn.models.DataBase.AppData;
 import io.github.AliAlmasiZ.tillDawn.models.GameAssetManager;
 import io.github.AliAlmasiZ.tillDawn.views.GameView;
 import io.github.AliAlmasiZ.tillDawn.views.PregameMenuView;
@@ -14,5 +18,30 @@ public class PregameMenuScreen extends ScreenAdapter {
     public PregameMenuScreen(Main main) {
         this.main = main;
         this.view = new PregameMenuView(GameAssetManager.getGameAssetManager().pixthulhuuiSkin);
+        view.start.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                GameScreen screen = new GameScreen(main);
+                main.setScreen(screen);
+                AppData.getAppData().activeUser.setLastGame(screen);
+                dispose();
+
+            }
+        });
+    }
+
+    @Override
+    public void render(float delta) {
+        view.render(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        view.resize(width, height);
+    }
+
+    @Override
+    public void show() {
+        view.show();
     }
 }
